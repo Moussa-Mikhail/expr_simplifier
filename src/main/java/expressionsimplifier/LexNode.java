9c7f454484b1f5821875caf4c658404c357767bf
Moussa Mikhail <moussamikhail56@gmail.com>
2023-03-11 19:38:56 -0600

    add equals and hashCode methods

--- src/main/java/expressionsimplifier/LexNode.java
+++ src/main/java/expressionsimplifier/LexNode.java
@@ -2,6 +2,8 @@ package expressionsimplifier;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 final class LexNode {
     public final @NotNull String token;
     public final @NotNull TokenType type;
@@ -14,5 +16,27 @@ final class LexNode {
     @Override
     public @NotNull String toString() {
         return token;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        LexNode lexNode = (LexNode) o;
+
+        if (!token.equals(lexNode.token)) {
+            return false;
+        }
+        return type == lexNode.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(token, type);
     }
 }
