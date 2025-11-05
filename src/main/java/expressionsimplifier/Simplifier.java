1aa04c9fd634bd38a3a50c853cbf78de4591efd1
Moussa <moussamikhail56@gmail.com>
2025-11-04 21:18:17 -0600

    Refactor to accept SyntaxTree as input.

--- src/main/java/expressionsimplifier/Simplifier.java
+++ src/main/java/expressionsimplifier/Simplifier.java
@@ -3,5 +3,5 @@ package expressionsimplifier;
 @SuppressWarnings("unused")
 @FunctionalInterface
 interface Simplifier {
-    SyntaxTree simplify(String operator, SyntaxTree left, SyntaxTree right);
+    SyntaxTree simplify(SyntaxTree tree);
 }
