c4fa1391d9b529ea50c35dd389d694821b94e8dd
Moussa <moussamikhail56@gmail.com>
2025-11-04 21:20:46 -0600

    Remove unnecessary ExpressionType variants.

--- src/main/java/expressionsimplifier/ExpressionType.java
+++ src/main/java/expressionsimplifier/ExpressionType.java
@@ -5,19 +5,13 @@ import java.util.Map;
 public enum ExpressionType {
     NUMBER,
     VARIABLE,
-    SUM,
-    DIFF,
-    PROD,
-    DIV,
-    POLY,
+    // Univariate Polynomials
+    UNIPOLY,
     EXP,
     // Catchall type for expressions that are not simple
     COMPLEX;
+
     public static final Map<String, ExpressionType> OPERATOR_TO_EXPRESSION_TYPE = Map.ofEntries(
-            Map.entry(Constants.ADD, SUM),
-            Map.entry(Constants.SUB, DIFF),
-            Map.entry(Constants.MUL, PROD),
-            Map.entry(Constants.DIV, DIV),
-            Map.entry(Constants.POW, POLY)
+            Map.entry(Constants.POW, UNIPOLY)
     );
 }
