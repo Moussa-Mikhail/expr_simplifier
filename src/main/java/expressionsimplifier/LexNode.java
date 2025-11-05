7e34be42e341c2b6049cd4b7578a43d35485d3e1
Moussa <moussamikhail56@gmail.com>
2025-11-04 21:19:46 -0600

    Add static members.

--- src/main/java/expressionsimplifier/LexNode.java
+++ src/main/java/expressionsimplifier/LexNode.java
@@ -8,6 +8,11 @@ import java.util.Objects;
 final class LexNode {
     public final @NotNull String token;
     public final @NotNull TokenType type;
+    public static final LexNode MUL = new LexNode(Constants.MUL, TokenType.OPERATOR);
+    public static final LexNode POW = new LexNode(Constants.POW, TokenType.OPERATOR);
+    public static final LexNode DIV = new LexNode(Constants.DIV, TokenType.OPERATOR);
+    public static final LexNode ZERO = new LexNode("0", TokenType.NUMBER);
+    public static final LexNode ONE = new LexNode("1", TokenType.NUMBER);
 
     public LexNode(String token, TokenType type) {
         this.token = token;
